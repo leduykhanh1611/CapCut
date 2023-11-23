9b917d8313ef3970b1aa66e8aa5cbd040a467e95
PhamDoanMinhTu <144188315+PhamDoanMinhTu@users.noreply.github.com>
2023-11-23 22:11:37 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import edit from './src/edit';
 import template from './src/template';
 import inbox from './src/inbox';
+import inbox2 from './src/inbox2';
 import me from './src/me';
 
 
@@ -12,10 +13,11 @@ export default function App() {
   const stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <stack.Navigator initialRouteName="me"  screenOptions={{ headerShown: false }}>
+      <stack.Navigator screenOptions={{ headerShown: false }}>
         <stack.Screen name='edit' component={edit} />
         <stack.Screen name='template' component={template} />
         <stack.Screen name='inbox' component={inbox} />
+        <stack.Screen name='inbox2' component={inbox2} />
         <stack.Screen name='me' component={me} />
       </stack.Navigator>
 
